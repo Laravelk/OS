d3a6c3f42f21627bafbc935ff6bc802928dab134
Laravelk <ivanmorozov1999@gmail.com>
2019-09-08 21:17:05 +0700

    update

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -10,12 +10,12 @@
 // http://rus-linux.net/MyLDP/algol/light-weight-processes-linux-threads.html
 // http://parallels.nsu.ru/WackoWiki/KursOperacionnyeSistemy/PraktikumPosixThreads/PthreadLectures/Intro2#h174-2
 // http://www.yolinux.com/TUTORIALS/LinuxTutorialPosixThreads.html
-// http://man7.org/linux/man-pages/man3/pthread_detach.3.html
 
 void *threadFunc(void *thread_data) {
   for (int i = 0; i < 10; ++i) {
     fprintf(stderr, "hello world in thread\n");
   }
+  pthread_exit(NULL);
 }
 
 void printCreateThreadError() {
@@ -35,38 +35,22 @@ void printCreateThreadError() {
   }
 }
 
-void printDetachThreadError() {
-  if (errno == EINVAL) {
-    fprintf(stderr, "thread is not a joinable thread");
-  }
-  if (errno == ESRCH) {
-    fprintf(stderr, "No thread with the ID thread could be found");
-  }
-}
-
 int main(int argc, char *argv[]) {
   void *threadData = NULL;
   pthread_t thread;
   pthread_attr_t attr;
-  pthread_attr_init(&attr); // default thread joinable
+  pthread_attr_init(&attr);
 
-  /* pthread_attr_setdetachstate(&attr, null) -- returns the detach state
-     attribute of the thread attributes object attr in the buffer pointed to by
-       detachstate */
+  for (int i = 0; i < 10; ++i) {
+    printf("hello world in main thread\n");
+  }
 
   if (pthread_create(&thread, &attr, threadFunc, threadData)) {
     printCreateThreadError();
     exit(EXIT_FAILURE);
   }
 
-  if (pthread_detach(thread)) {
-    printDetachThreadError();
-    exit(EXIT_FAILURE);
-  }
+  pthread_attr_destroy(&attr);
 
-  for (int i = 0; i < 10; ++i) {
-    printf("hello world in main thread\n");
-  }
-
-  return 0;
+  pthread_exit(NULL);
 }
